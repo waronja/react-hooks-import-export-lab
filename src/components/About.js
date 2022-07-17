4982c24314ef77ef7b180f4c43e550a8e1c6146d
simon waronja <simon.waronja@student.moringaschool.com>
2022-07-17 18:48:07 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {image} from "../data/user" ;
+
 
 function About() {
   return (
@@ -9,3 +11,5 @@ function About() {
     </div>
   );
 }
+
+export default About;
